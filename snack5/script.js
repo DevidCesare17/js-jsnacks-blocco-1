18a567c14c5420d872ecdb0c0e77fa7feeca20f0
Davide Cesare <davidereaper@gmail.com>
2020-08-05 15:25:44 +0200

    Fine snack5

---
+++ snack5/script.js
@@ -0,0 +1,9 @@
+// 5. chiedere all'utente di inserire un numero, e se il numero è pari viene visualizzato nel box rosso, altrimenti in quello verde.
+
+var numero = parseInt(prompt("Inserisci un numero"));
+
+if (numero % 2 == 0) {
+  document.getElementById("numero").innerHTML = numero;
+} else {
+  document.getElementById("numero_1").innerHTML = numero;
+}
